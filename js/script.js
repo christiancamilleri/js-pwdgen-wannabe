e068fa18950f68b1fa4fc6f1ea7152acbf53d5a5
christiancamilleri <camilleri.christian@outlook.com>
2023-02-21 15:05:26 +0100

    Exercise complete

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi +/
+
+/* 
+- chiedi il nome
+- chiedi il cognome 
+- chiedi il colore preferito 
+-inserisci tutto in pagina 
+*/
+
+let userName;
+
+userName = prompt("Qual è il tuo nome?")
+
+let userSurname;
+
+surName = prompt("Qual è il tuo cognome?")
+
+let favoriteColor; 
+
+favoriteColor = prompt("Qual è il tuo colore preferito?")
+
+document.writeln( userName  + surName  + favoriteColor  + 3 )
+
+
